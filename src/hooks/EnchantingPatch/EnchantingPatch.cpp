5e9b0cecf05b6151e0df00443fb061d43ed40c70
BuildTools <unconfigured@null.spigotmc.org>
2025-03-01 22:45:25 +0800

    Fixed enchantment not showing as selected

--- src/hooks/EnchantingPatch/EnchantingPatch.cpp
+++ src/hooks/EnchantingPatch/EnchantingPatch.cpp
@@ -24,15 +24,31 @@ void EnchantingPatch::PostMessage_(RE::MessageBoxData* data)
 
     data->callback->Run(RE::IMessageBoxCallback::Message::kUnk0);
 
+    static REL::Relocation<void(*)(void*, void*, bool)> update{ RELOCATION_ID(50320, 51234) };
+
     for (auto&& eff : savedEffects) {
-        eff->selected = true;
+
+        for (uint32_t idx = 0; idx < menu->listEntries.size(); idx++) {
+            auto&& it = menu->listEntries[idx];
+            if (it && it->filterFlag.any(RE::CraftingSubMenus::EnchantConstructMenu::FilterFlag::EffectArmor, RE::CraftingSubMenus::EnchantConstructMenu::FilterFlag::EffectWeapon) && (*(RE::BSTSmartPointer<RE::CraftingSubMenus::EnchantConstructMenu::EnchantmentEntry>*) & it)->data->formID == eff->data->formID) {
+                auto&& effect = (*(RE::BSTSmartPointer<RE::CraftingSubMenus::EnchantConstructMenu::EnchantmentEntry>*)&it);
+                effect->magnitude = eff->magnitude;
+                effect->selected = true;
+                menu->selected.effects.push_back(effect);
+                break;
+            }
+        }
+    }
+    for (uint32_t idx = 0; idx < menu->listEntries.size(); idx++) {
+        auto&& it = menu->listEntries[idx];
+        if (it && it->filterFlag.all(RE::CraftingSubMenus::EnchantConstructMenu::FilterFlag::SoulGem) && (*(RE::BSTSmartPointer<RE::CraftingSubMenus::EnchantConstructMenu::ItemChangeEntry>*) & it)->data->GetObject() == soulGemObj) {
+            menu->selected.soulGem = (*(RE::BSTSmartPointer<RE::CraftingSubMenus::EnchantConstructMenu::ItemChangeEntry>*) & it);
+            it->selected = true;
+        }
     }
 
-    menu->selected.effects = savedEffects;
-    for (auto&& it : menu->listEntries) {
-        if (it && it->filterFlag.all(RE::CraftingSubMenus::EnchantConstructMenu::FilterFlag::SoulGem) && (*(RE::BSTSmartPointer<RE::CraftingSubMenus::EnchantConstructMenu::ItemChangeEntry>*) & it)->data->GetObject() == soulGemObj)
-            menu->selected.soulGem = *(RE::BSTSmartPointer<RE::CraftingSubMenus::EnchantConstructMenu::ItemChangeEntry>*)&it;
-    }
+    
+    update(menu, &menu->listEntries, false);
 
     return;
 }
